93914eeb64768c6a447cfa046afbaaaf43781ff7
aaronum0224 <aaronuo2008@gmail.com>
2026-06-10 19:13:11 -0600

    Resuelve ejercicios 2 con pila

--- EjerciciosRecursivosPersona2.py
+++ EjerciciosRecursivosPersona2.py
@@ -7,3 +7,14 @@ def sumar_digitos(num):
     if num < 10:
         return num
     return num % 10 + sumar_digitos(num // 10)
+
+# Ejercicio 2 PILA
+def contar_pares(num):
+    if num < 10:
+        if num % 2 == 0:
+            return 1
+        return 0
+    if (num % 10) % 2 == 0:
+        return 1 + contar_pares(num // 10)
+    return contar_pares(num // 10)
+
